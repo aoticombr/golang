90a32e8147a09c05a91395ea335488beb7aaee88
krobrelus <paulo@aoti.com.br>
2023-06-07 12:32:28 -0400

    commit 9

--- dataset/field.go
+++ dataset/field.go
@@ -2,6 +2,9 @@ package dataset
 
 import (
 	sql "database/sql"
+	"fmt"
+	"reflect"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -20,79 +23,105 @@ type Field struct {
 }
 
 func (field Field) AsString() string {
-	return field.Value.(string)
+
+	switch fieldValue := field.Value.(type) {
+	case time.Time:
+		layout := "02/01/2006 15:04:05"
+		formattedTime := fieldValue.Format(layout)
+		if strings.Contains(formattedTime, "00:00:00") {
+			formattedTime = formattedTime[:10]
+		}
+		return formattedTime
+	case string:
+		return fieldValue
+	default:
+		t := reflect.TypeOf(field.Value)
+		s := fmt.Sprintf("Unable to convert data type to string,Tipo: %v\n ", t)
+		panic(s)
+	}
 }
 
 func (field Field) AsInt() int {
-	if field.Value != nil {
-		switch field.Value.(type) {
-		case int:
-			return field.Value.(int)
-		case int8:
-			return int(field.Value.(int8))
-		case int16:
-			return int(field.Value.(int16))
-		case int32:
-			return int(field.Value.(int32))
-		case int64:
-			return int(field.Value.(int64))
-		default:
-			panic("unable to convert data type to int")
-		}
-	} else {
-		return 0
+	switch value := field.Value.(type) {
+	case int:
+		return int(value)
+	case int8:
+		return int(int8(value))
+	case int16:
+		return int(int16(value))
+	case int32:
+		return int(int32(value))
+	case int64:
+		return int(int64(value))
+	default:
+		panic("Unable to convert data type to int")
 	}
 }
 
 func (field Field) AsInt64() int64 {
-	if field.Value != nil {
-		switch field.Value.(type) {
-		case int:
-			return int64(field.Value.(int))
-		case int8:
-			return int64(field.Value.(int8))
-		case int16:
-			return int64(field.Value.(int16))
-		case int32:
-			return int64(field.Value.(int32))
-		case int64:
-			return field.Value.(int64)
-		default:
-			panic("Unable to convert data type to int")
+	switch value := field.Value.(type) {
+	case int:
+		return int64(field.Value.(int))
+	case int8:
+		return int64(value)
+	case int16:
+		return int64(value)
+	case int32:
+		return int64(value)
+	case int64:
+		return value
+	case string:
+		valueInt, err := convertStringToInt64(value)
+		if err != nil {
+			return 0
 		}
-	} else {
-		return int64(0)
+		return valueInt
 	}
+	return 0
 }
 
 func (field Field) AsFloat() float32 {
-	if field.Value != nil {
-		switch field.Value.(type) {
-		case float32:
-			return field.Value.(float32)
-		case float64:
-			return float32(field.Value.(float64))
-		default:
-			panic("Unable to convert data type to float32")
+	switch value := field.Value.(type) {
+	case float32:
+		return value
+	case float64:
+		return float32(value)
+	case string:
+		floatValue, err := strconv.ParseFloat(value, 32)
+		if err != nil {
+			panic("Erro ao converter valor string para float32")
 		}
-	} else {
-		return float32(0)
+		return float32(floatValue)
+	default:
+		t := reflect.TypeOf(field.Value)
+		s := fmt.Sprintf("Unable to convert data type to float32,Tipo: %v\n ", t)
+		panic(s)
 	}
 }
 
-func (field Field) AsFloat64() float64 {
-	if field.Value != nil {
-		switch field.Value.(type) {
-		case float32:
-			return float64(field.Value.(float32))
-		case float64:
-			return field.Value.(float64)
-		default:
-			panic("Unable to convert data type to float64")
-		}
-	} else {
-		return float64(0)
+func convertStringToInt64(value string) (int64, error) {
+	number, err := strconv.Atoi(value)
+	if err != nil {
+		return 0, err
 	}
+	return int64(number), nil
+}
+
+func (field Field) AsFloat64() float64 {
+
+	switch value := field.Value.(type) {
+	case float32:
+		return float64(float32(value))
+	case float64:
+		return value
+	case string:
+		floatValue, err := strconv.ParseFloat(value, 32)
+		if err != nil {
+			panic("Erro ao converter valor string para float32")
+		}
+		return float64(floatValue)
+	}
+	return 0
 }
 
 func (field Field) AsBool() bool {
