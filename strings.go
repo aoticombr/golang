f7b8b1885b3dc3d3dede6f911cb21a64989c3f69
aoticombr <krobrelus@gmail.com>
2023-06-09 17:29:00 -0400

    Create strings.go

---
+++ strings.go
@@ -0,0 +1,28 @@
+package types
+
+import (
+	"strings"
+)
+
+type Strings struct {
+	itens []string
+}
+
+func (s *Strings) Append(value string) *Strings {
+	s.itens = append(s.itens, value)
+	return s
+}
+func (s *Strings) Clear() *Strings {
+	s.itens = nil
+	return s
+}
+func (s *Strings) Add(value string) *Strings {
+	s.itens = append(s.itens, value)
+	return s
+}
+func (s *Strings) Count() int {
+	return len(s.itens)
+}
+func (s *Strings) Text() string {
+	return strings.Join(s.itens, " ")
+}
