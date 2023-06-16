78ae021f6788604e11fc39dc7b28e8530b9eb3c6
aoticombr <krobrelus@gmail.com>
2023-06-16 08:27:05 -0400

    Update variant.go

--- component/variant.go
+++ component/variant.go
@@ -1,27 +1,23 @@
 package component
 
 import (
-	"fmt"
+	"log"
 	"reflect"
 	"strconv"
 	"strings"
 	"time"
 )
-type Variant struct {
-	Value interface{}
+
+type variant struct {
+	Value any
 }
 
-// type Variant struct {
-// 	Value interface{}
-// }
-func (v Variant) SetValue(value interface{}){
-	v.Value = value
-}
-func (v Variant) AsValue() interface{}{
-  return	v.Value 
+func (v variant) AsValue() any {
+	return v.Value
 }
 
-func (v Variant) AsString() string {
+func (v variant) AsString() string {
+
 	switch val := v.Value.(type) {
 	case time.Time:
 		layout := "02/01/2006 15:04:05"
@@ -30,39 +26,66 @@ func (v Variant) AsString() string {
 			formattedTime = formattedTime[:10]
 		}
 		return formattedTime
+	case nil:
+		return ""
 	case string:
 		return val
 	default:
 		t := reflect.TypeOf(v.Value)
-		panic(fmt.Sprintf("Unable to convert data type to string, Tipo: %v\n", t))
+		log.Printf("unable to convert data type to string. Type: %v", t)
+		return ""
 	}
+
 }
 
-func (v Variant) AsInt() int {
-	switch val := v.Value.(type) {
-	case int, int8, int16, int32, int64:
-		return int(reflect.ValueOf(val).Int())
-	default:
-		panic("Unable to convert data type to int")
-	}
-}
+func (v variant) AsInt() int {
 
-func (v Variant) AsInt64() int64 {
 	switch val := v.Value.(type) {
-	case int, int8, int16, int32, int64:
-		return reflect.ValueOf(val).Int()
-	case string:
-		valueInt, err := strconv.ParseInt(val, 10, 64)
-		if err != nil {
-			return 0
-		}
-		return valueInt
+	case int:
+		return v.Value.(int)
+	case int8:
+		return int(v.Value.(int8))
+	case int16:
+		return int(v.Value.(int16))
+	case int32:
+		return int(v.Value.(int32))
+	case int64:
+		return int(v.Value.(int64))
+	case nil:
+		return 0
 	default:
+		t := reflect.TypeOf(val)
+		log.Printf("unable to convert data type to int, type: %v", t)
 		return 0
 	}
+
 }
 
-func (v Variant) AsFloat() float32 {
+func (v variant) AsInt64() int64 {
+
+	switch val := v.Value.(type) {
+	case int:
+		return int64(v.Value.(int))
+	case int8:
+		return int64(v.Value.(int8))
+	case int16:
+		return int64(v.Value.(int16))
+	case int32:
+		return int64(v.Value.(int32))
+	case int64:
+		return v.Value.(int64)
+	case nil:
+		return int64(0)
+	default:
+		t := reflect.TypeOf(val)
+		log.Printf("unable to convert data type to int64, type: %v", t)
+		return int64(0)
+	}
+
+}
+
+func (v variant) AsFloat() float32 {
+
 	switch val := v.Value.(type) {
 	case float32:
 		return val
@@ -71,16 +94,23 @@ func (v Variant) AsFloat() float32 {
 	case string:
 		floatValue, err := strconv.ParseFloat(val, 32)
 		if err != nil {
-			panic("Error converting string value to float32")
+			t := reflect.TypeOf(val)
+			log.Printf("unable to convert data type to float32, type: %v", t)
+			return float32(0)
 		}
 		return float32(floatValue)
+	case nil:
+		return float32(0)
 	default:
 		t := reflect.TypeOf(val)
-		panic(fmt.Sprintf("Unable to convert data type to float32, Tipo: %v\n", t))
+		log.Printf("unable to convert data type to float32, type: %v", t)
+		return float32(0)
 	}
+
 }
 
-func (v Variant) AsFloat64() float64 {
+func (v variant) AsFloat64() float64 {
+
 	switch val := v.Value.(type) {
 	case float32:
 		return float64(val)
@@ -89,37 +119,63 @@ func (v Variant) AsFloat64() float64 {
 	case string:
 		floatValue, err := strconv.ParseFloat(val, 64)
 		if err != nil {
-			panic("Error converting string value to float64")
+			t := reflect.TypeOf(val)
+			log.Printf("unable to convert data type to float64, type: %v", t)
+			return float64(0)
 		}
 		return floatValue
+	case nil:
+		return float64(0)
 	default:
-		return 0
+		t := reflect.TypeOf(val)
+		log.Printf("unable to convert data type to float64, type: %v", t)
+		return float64(0)
 	}
+
 }
 
-func (v Variant) AsBool() bool {
-	if v.Value != nil {
-		switch val := v.Value.(type) {
-		case int, int8, int16, int32, int64:
-			return reflect.ValueOf(val).Int() == 1
-		case string:
-			v := strings.ToUpper(strings.TrimSpace(val))
-			return v == "1" || v == "S" || v == "Y"
-		default:
-			panic("Unable to convert data type to bool")
+func (v variant) AsBool() bool {
+
+	switch val := v.Value.(type) {
+	case int:
+		return v.Value.(int) == 1
+	case int8:
+		return v.Value.(int8) == 1
+	case int16:
+		return v.Value.(int16) == 1
+	case int32:
+		return v.Value.(int32) == 1
+	case int64:
+		return v.Value.(int64) == 1
+	case string:
+		value := strings.ToUpper(strings.Trim(v.Value.(string), " "))
+		if value == "1" || value == "S" || value == "Y" {
+			return true
+		} else {
+			return false
 		}
+	case nil:
+		return false
+	default:
+		t := reflect.TypeOf(val)
+		log.Printf("unable to convert data type to bool, type: %v", t)
+		return false
 	}
-	return false
+
 }
 
-func (v Variant) AsDateTime() time.Time {
-	if v.Value != nil {
-		switch val := v.Value.(type) {
-		case time.Time:
-			return val
-		default:
-			panic("Unable to convert data type to time.Time")
-		}
+func (v variant) AsDateTime() time.Time {
+
+	switch v.Value.(type) {
+	case time.Time:
+		return v.Value.(time.Time)
+	case nil:
+		data, _ := time.Parse(time.DateTime, time.DateTime)
+		return data
+	default:
+		log.Printf("unable to convert data type to time.")
+		data, _ := time.Parse(time.DateTime, time.DateTime)
+		return data
 	}
-	return v.Value.(time.Time)
+
 }
