7c25af5a5c08b608533ce903809471bd1a4436dc
PAULO <krobrelus@gmail.com>
2023-07-26 13:26:31 -0400

    up3

--- testes/jsonconfig/exemplo_json.go
+++ testes/jsonconfig/exemplo_json.go
@@ -6,7 +6,21 @@ import (
 
 func main() {
 
-	json.NewJsonConfigGlobal().Name = "config.json"
-	json.NewJsonConfigGlobal().Load()
+	js := json.NewJsonConfigGlobal()
+	js.Name = "config.json"
+	js.Load()
+
+	for key, boot := range js.GetConfig().Boots {
+		println("Boot ", key, ": ", boot.Name)
+		for key, schema := range boot.GetSchemas() {
+			println("Schema ", key, ": ", schema.Schema)
+		}
+	}
+	for key, api := range js.GetConfig().Apis {
+		println("Api ", key, ": ", api.Name)
+		for key, schema := range api.GetSchemas() {
+			println("Schema ", key, ": ", schema.Schema)
+		}
+	}
 
 }
