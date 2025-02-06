6ef9384df2e4b46e0d90a19dff35869f17f9b733
krobrelus <krobrelus@gmail.com>
2025-02-06 15:32:39 -0400

    ajuste no teste

--- testes/http/comp_test.go
+++ testes/http/comp_test.go
@@ -71,14 +71,13 @@ func TestSendRaw(t *testing.T) {
 	fmt.Println("Teste")
 	cp := http.NewHttp()
 	cp.SetUrl("http://127.0.0.1:3003")
-	///	cp.UserName = "thiago.silva@nbsi.com.br"
-	///	cp.Password = "Paymail01@"
+
 	cp.Metodo = http.M_POST
 	cp.EncType = http.ET_RAW
 	cp.Request.Header.ContentType = "application/json"
 	cp.Request.AddFormField("grant_type", "client_credentials")
 	cp.Request.Body = []byte(`{
-	 	"user":"admin@aoti.com.br",
+	 	"user":"admin@teste.com.br",
 	 	"pass":"master"
 	 }	`)
 	resp, err := cp.Send()
@@ -292,8 +291,8 @@ func TestWebSocket(t *testing.T) {
 	rs = &ReadSocket{}
 	fmt.Println("Teste")
 	cp := http.NewHttp()
-	cp.Request.Header.AddField("x-vectury-dealer", "07600973")
-	cp.Authorization = `eyJraWQiOiJEVXBTQWFUREtQRHphS19mV0NJcldxUUVOVTQ4bzM2ZXR5ZlV5eG9PaFY0IiwiYWxnIjoiUlMyNTYifQ.eyJ2ZXIiOjEsImp0aSI6IkFULklIUGRpQ0JtbkhxX0pjcDNSeG56bHY1UkozR0pjbDFudXJjVmlwckxMcVUiLCJpc3MiOiJodHRwczovL3Nzby11YXQucmVuYXVsdC5jb20vb2F1dGgyL2F1c3R3b2VzaGJDa1BKeXcxNDE2IiwiYXVkIjoiaHR0cHM6Ly9hcGlzLnJlbmF1bHQuY29tIiwiaWF0IjoxNjk4MTc0NzY1LCJleHAiOjE2OTgxNzgzNjUsImNpZCI6Imlybi03MDcyNV91YXRfcGtqd3Rfb3VxZHN2Z2NqczNxIiwic2NwIjpbImFwaXMuZGVmYXVsdCIsImRmdC12MS5kZWFsZXJzLWRvd25sb2FkIiwiZGZ0LXYxLmRlYWxlcnMtdXBsb2FkIl0sInN1YiI6Imlybi03MDcyNV91YXRfcGtqd3Rfb3VxZHN2Z2NqczNxIiwiaXJuIjoiSVJOLTcwNzI1IiwiY2VydC11aWQiOiJpcm4tNzA3MjVfdWF0X3Brand0In0.J66bg1F8UpHJOhfus28aNs3_YmRvhaU0Y7KVpEPVbsjtr8mkuNS3ulMVvZp9ba3yw94AMMT_aolWM2qgzRWvas9ugYNebBJez_B5Nj8SPdQF34HJxC_FEINCfd6IqRDF-NhlaMpbVgvycc8DwzM2Jq_5YZ2P1VQryOtze07iXSbGm7HGwRGjX_e7_0nrsQ0P5_AxHmPHWSVZRMJukl4dA6LjwViyD5U8ZCkwSkcfPXJfv37cg693iTldW098ZNJdAM0yBr1dj0Ig_vR_LCFc3YPWLi_KfwpfHPgB5xFClUOUuMXW18KX7-xi1gkL2gBIibb3WGcg8pDxP6Gl3Syhjg`
+	cp.Request.Header.AddField("x-xxx-dealer", "07600973")
+	cp.Authorization = `eyJg`
 	cp.AuthorizationType = http.AT_Bearer
 	cp.SetUrl("ws://localhost:3030/route1")
 
